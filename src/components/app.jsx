cb0ac9598929ee397438279ebb167b56b9b527c4
David Tang <david_tang@web.de>
2025-09-30 11:18:29 +0200

    Left/Right Scene, App Setup, Gif Done

---
+++ src/components/app.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import Gif from './gif';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedGifId: "Z1awblkp2wMMGtx3wB",
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="left-scene">
+          <input className="form-search" type="text" />
+          <div className="selected-gif">
+            <Gif gifId={this.state.selectedGifId} />
+          </div>
+        </div>
+        <div className="right-scene">
+          <div className="gif-list">
+            <img className='gif' src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExNXIyaTJvZDV2NXVydHozcmgyOHU2bHpyMWg4d3F2cDF3OWVreHRjbiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/swGGWrTpKIJLzIZ9az/giphy.gif" alt="" />
+            <img className='gif' src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExNXIyaTJvZDV2NXVydHozcmgyOHU2bHpyMWg4d3F2cDF3OWVreHRjbiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/swGGWrTpKIJLzIZ9az/giphy.gif" alt="" />
+            <img className='gif' src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExNXIyaTJvZDV2NXVydHozcmgyOHU2bHpyMWg4d3F2cDF3OWVreHRjbiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/swGGWrTpKIJLzIZ9az/giphy.gif" alt="" />
+            <img className='gif' src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExNXIyaTJvZDV2NXVydHozcmgyOHU2bHpyMWg4d3F2cDF3OWVreHRjbiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/swGGWrTpKIJLzIZ9az/giphy.gif" alt="" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/gif.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+// eslint-disable-next-line react/prefer-stateless-function
+class Gif extends Component {
+  render() {
+    const src = `https://media4.giphy.com/media/${this.props.gifId}/giphy.gif`;
+
+    return (
+      <img className="gif" src={src} alt="" />
+    );
+  }
+}
+
+export default Gif;
